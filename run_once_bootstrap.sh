39e16055295bd680b9794ec869264b25355c8d31
Eddie LeBlanc <eddie.leblanc@gmail.com>
2025-03-18 10:00:37 -0500

    cleaned up bootstrap script

--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-
-sudo apt install git curl
-curl -fsSL https://tailscale.com/install.sh | sh
-
+# install requirements and assumes chemoi alreadyinstalled
+sudo apt install git curl -y
+#curl -fsSL https://tailscale.com/install.sh | sh
+# install nix packages
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
   sh -s -- install
-
+# activate nix packages in current shell
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-
+# install home manager and apply config
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix run home-manager/master -- init --switch
