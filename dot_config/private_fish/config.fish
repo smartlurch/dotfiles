c38a92ad73f28b047fd8fb46d3d4f78b1a6ebbb1
Eddie LeBlanc <eddie.leblanc@gmail.com>
2025-03-18 09:49:27 -0500

    added zoxide to fish config

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-
+zoxide init fish | source
 eval (dircolors /home/lurch/.dir_colors/dircolors | head -n 1 | sed 's/^LS_COLORS=/set -x LS_COLORS /;s/;$//')
