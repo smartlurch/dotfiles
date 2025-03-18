df494d9f3ca44be18578b1010687a0b7330ecb74
Eddie LeBlanc <eddie.leblanc@gmail.com>
2025-03-17 21:29:52 -0500

    fix bug in bootstrap.sh

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -21,6 +21,7 @@
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
     # pkgs.hello
+    pkgs.glances
     pkgs.duf
     pkgs.tealdeer
     pkgs.yazi
--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -6,6 +6,8 @@ curl -fsSL https://tailscale.com/install.sh | sh
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
   sh -s -- install
 
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
 nix-channel --update
 nix run home-manager/master -- init --switch
