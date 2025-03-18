494c0f0e82dbdff8183201986ccc6a7aa1026d2e
Eddie LeBlanc <eddie.leblanc@gmail.com>
2025-03-18 10:41:08 -0500

    add fish to home nix and remove prompts from bootstrap.sh

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -18,6 +18,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
+    pkgs.fish
     pkgs.glances
     pkgs.duf
     pkgs.tealdeer
--- run_once_bootstrap.sh
+++ run_once_bootstrap.sh
@@ -3,8 +3,7 @@
 sudo apt install git curl -y
 #curl -fsSL https://tailscale.com/install.sh | sh
 # install nix packages
-curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | \
-  sh -s -- install
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install --determinate 
 # activate nix packages in current shell
 . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 # install home manager and apply config
